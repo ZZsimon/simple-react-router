9bb5fe93589b9ea476730f46856e16bf9e18fb1e
邵玉棋 <shaoyuqi@hujiang.com>
2021-11-16 14:15:51 +0800

    update README

--- src/simple-react-router/index.js
+++ src/simple-react-router/index.js
@@ -1,6 +1,6 @@
 
 export { default as BrowserRouter } from  "./BrowserRouter";
+export { default as Router } from  "./Router";
 export { default as Switch } from  "./Switch";
 export { default as Route } from  "./Route";
 export { default as Link } from  "./Link";
-export { default as Router } from  "./Router";
